4ea1e42692f49ed4e38b217e68bc222ace72017e
moisesmv16 <144186597+moisesmv16@users.noreply.github.com>
2025-06-03 08:21:59 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import server from "./server";
 import colors from "colors";
 
 
-server.listen(4000, () => {
+server.listen(4001, () => {
     console.log(colors.cyan.bold("REST API en el puerto 4000"))
 })
